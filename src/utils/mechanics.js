62d0214d55a55382e7e37ce5fbdf3d54c2240bae
McQuade <mcquxde@gmail.com>
2025-12-05 10:36:21 -0500

    Create mechanics.js

---
+++ src/utils/mechanics.js
@@ -0,0 +1,11 @@
+// src/utils/mechanics.js
+import { RACES, CLASSES } from '../config/gameData';
+
+export const getEffectiveStats = (unit) => {
+  // ... paste your logic here ...
+  return stats;
+};
+
+export const generateRecruit = () => {
+  // ... paste logic here ...
+};
